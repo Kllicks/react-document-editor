c955601965ac0826861bfd4e31e08d0cccf0d355
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-08 00:03:30 -0500

    finishes up the current version of document editor from chris

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -7,24 +7,91 @@ import EditorWindow from './EditorWindow';
 class NotesApp extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            currentNoteId: 1003,
+            searchTerm: '',
+            notes: [
+                {
+                    id: 1001,
+                    title: 'the la la note',
+                    content: 'la la la',
+                },
+                {
+                    id: 1002,
+                    title: 'the wa wa note',
+                    content: 'wa wa wa',                    
+                },
+                {
+                    id: 1004,
+                    title: 'lady',
+                    content: 'ga ga'
+                },
+            ]
+        }
     }
     render() {
         return (
           <div className="notes-app">
-            <SearchBar />
-            <NotesList 
-                notes={[
-                    'note #1',
-                    'note #2',
-                    'note #3',
-                    'note #4',
-                    'note #5',
-                ]}
+            <SearchBar
+                searchTerm={this.state.searchTerm}
+                handleInput={this._setSearchTerm}
             />
-            <EditorWindow />
+            <NotesList 
+                notes={this._searchNotes(this.state.searchTerm)}
+                handleClick={this._setCurrentNoteId}
+            />
+            <EditorWindow 
+                content={this._getById(this.state.currentNoteId).content}
+            />
           </div>
 
         );
+    }
+
+    _setSearchTerm = (term) => {
+        console.log(`they said: ${term}`);
+        this.setState({
+            searchTerm: term
+        });
+    }
+
+    _setCurrentNoteId = (noteId) => {
+        console.log(`The child said this was the id: ${noteId}`);
+        this.setState({
+            currentNoteId: noteId
+        });
+    }
+
+    _getById = (idToFind) => {
+        // find the object in this.state.notes where id === idToFind
+        const theOne = this.state.notes.find(note => {
+            return note.id === idToFind
+        });
+        return theOne || { content: ''};
+        /*
+            if (theOne) {
+                return theOne;
+            } else {
+                const dummyObject = {
+                    content: ''
+                };
+                return dummyObject;
+            }
+        */
+    }
+
+    _searchNotes = (term) => {
+        // filter out any notes
+        // that do not include the term
+        // in either the title or content
+        const filteredNotes = this.state.notes.filter(note => {
+            const termIsInTitle = note.title.includes(term);
+            const termIsInContent = note.content.includes(term);
+
+            return termIsInTitle || termIsInContent;
+        });
+
+        return filteredNotes;
     }
 }
 
--- src/NotesList.js
+++ src/NotesList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const NotesList = (props) => {
-    console.log(props.notes);
+    // console.log(props.notes);
     return (
         <ul className="notes-list list-group">
           {
             props.notes.map(note => {
-                return (<li className="list-group-item"><a>{note}</a></li>)
+                return (
+                    <li className="list-group-item"
+                        onClick={() => {
+                            console.log(note.id);
+                            props.handleClick(note.id);
+                    }}>
+                            {note.title}
+                    </li>)
             })
           }          
         </ul>
